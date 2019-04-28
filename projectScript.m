9b3f87795a4c976886de86ff70eef39ebb0acee7
Oreofe <oaderibigbe22@gmail.com>
2019-04-28 17:57:02 -0400

    audio recorder object

--- projectScript.m
+++ projectScript.m
@@ -3,7 +3,10 @@
 %% Create music input using audiorecorder
 
 %% Create ambient noise input using audiorecorder
+r = audiorecorder(44100,16,1);
 
+recordblocking(r,10);
+play(r);
 %% generate noise
 
 %% Sum
